021eb48111d1ac0d0ed141b9af73c2f5511e0c3a
Manuel Pardo <42596887+mapardoo@users.noreply.github.com>
2024-08-26 22:51:46 -0500

    Update mapping_merge_classes.py

--- mapping_merge_classes.py
+++ mapping_merge_classes.py
@@ -14,3 +14,13 @@ class_mapping = {
     'Cell': 'Cell',
     'Cracking': 'Cracking'
 }
+
+# Load metadata from JSON. Remapping classes
+def cargar_metadatos(path_file, class_mapping):
+    with open(path_file, 'r') as file:
+        metadata = json.load(file)
+    for key in metadata.keys():
+        original_class = metadata[key]['anomaly_class']
+        if original_class in class_mapping:
+            metadata[key]['anomaly_class'] = class_mapping[original_class]
+    return metadata
